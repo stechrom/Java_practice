e14e27d8aefcced6aa2c5f67f754f5b7d8266b3d
Roman Stechkevych <roman.stechkevych@tietoevry.com>
2021-07-07 16:51:23 +0300

    added [][] array to learning class on dev branch

--- src/Learning.java
+++ src/Learning.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Learning {
     public static void main(String[] args) {
         System.out.println("Let's start learning java OOP");
+
+        int[][] newArray = {
+                {1,1,1},{2,2,2}
+        };
+        System.out.println("This is a new array: " + "\n" + Arrays.deepToString(newArray));
     }
 }
