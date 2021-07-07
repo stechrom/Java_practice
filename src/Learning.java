07bb3838937e810b745210741ce6f259de2a4e35
Roman Stechkevych <roman.stechkevych@tietoevry.com>
2021-07-07 17:54:57 +0300

    added LearningInterfaces new class

--- src/Learning.java
+++ src/Learning.java
@@ -8,5 +8,7 @@ public class Learning {
                 {1,1,1},{2,2,2}
         };
         System.out.println("This is a new array: " + "\n" + Arrays.deepToString(newArray));
+
+        System.out.println("LearningInterfaces class added to push to GitHub");
     }
 }
